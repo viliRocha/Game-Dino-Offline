ec8fd7c5f76486dd837526f8e7efe544bfe2334c
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-08-01 02:44:25 +0000

    ok

--- static/script/sketch.js
+++ static/script/sketch.js
@@ -36,15 +36,15 @@ let trex,
     onGround = true;
 
 function preload() {
-  //Pterodactylus dinossaur enemy flying animation
+  // Pterodactylus dinossaur enemy flying animation
   texFlyingDino = loadAnimation("/assets/bird1.png", "/assets/bird2.webp");
 
-  //Trex animations
+  // Trex animations
   texTrex = loadAnimation("/assets/trex1.png", "/assets/trex2.png", "/assets/trex3.webp", "/assets/trex4.webp");
   trexSprint = loadAnimation("/assets/TrexDown1.png", "/assets/TrexDown2.png");
   trexCollide = loadImage("/assets/collide.webp");
 
-  //Player sound effects
+  // Player sound effects
   jumpingSound = loadSound("/sounds/collided.wav");
   collideSound = loadSound("/sounds/jump.wav");
 }
@@ -64,12 +64,12 @@ function setup() {
   texTrex.frameDelay = 5;
   trexSprint.frameDelay = 10;
 
-  //Loading the game over text that will apear when player dies
+  // Loading the game over text that will apear when player dies
   gameOverTxt = createImg("/assets/gameOver.webp");
   gameOverTxt.position(windowWidth / 2 - 254, windowHeight / 2 - 35);
   gameOverTxt.size(450, 45);
 
-  //creating a restart button that will only be shown when player dies
+  // reating a restart button that will only be shown when player dies
   restart = createImg("/assets/restart.png");
   restart.position(windowWidth / 2 - 50, windowHeight / 2 + 25);
   restart.size(50, 50);
@@ -81,7 +81,7 @@ function setup() {
 
   trex.scale = 1;
 
-  //Defining player's weight, player's mass is 0 because otherwise, it would flip the floor
+  // Defining player's weight, player's mass is 0 because otherwise, it would flip the floor
   trex.mass = 0;
 
   ground = new Sprite(windowWidth / 2, windowHeight - 30, windowWidth, 50);
@@ -91,20 +91,20 @@ function setup() {
   ground.image.offset.y = -30;
   ground.scale = 0.9;
 
-  //Removing friction between Trex and ground
+  // Removing friction between Trex and ground
   ground.friction = 0;
 
-  //This way I can set the flyingDinosaurs height as low as I want
+  // This way I can set the flyingDinosaurs height as low as I want
   ground.collider = 'kinematic';
 
-  //ground.debug = true;
+  // ground.debug = true;
 
   moon = new Sprite(windowWidth/2 + 300,windowHeight/2 - 120, 50, 50);
   moon.image = "/assets/moon.webp";
   moon.scale = 0.15;
   moon.collider = 'none';
 
-  //Defining font style
+  // Defining font style
   textFont('Fira Code', 27);
 
   cloudsGroup = new Group();
@@ -117,17 +117,17 @@ function draw() {
 
   moon.tint = color(255, 255, 255, moonOpacity);
 
-  //Show player score and score record in screen
+  // Show player score and score record in screen
   fill("white");
   text(score, windowWidth - 1200, windowHeight - 530);
   text("Maximum score: " + maximumScore, windowWidth - 500, windowHeight - 530);
 
-  //If player is already up in the air it can't jump
+  // If player is already up in the air it can't jump
   if (!trex.collides(ground)) {
     trex.velocity.y += 1;
   }
 
-  //Give the impression player is moving
+  // Give the impression player is moving
   // my idea: make the ground animation move and not the object itself
   ground.velocity.x = -8;
 
@@ -142,7 +142,7 @@ function draw() {
     restart.hide();
     gameOverTxt.hide();
 
-    //Generate a new ground in front of the other one otherwise player would fall(ground has speed)
+    // Generate a new ground in front of the other one otherwise player would fall(ground has speed)
     // with my idea, the ground animation moves and not the object itself, so this wouldn't be necessary
     if (ground.x <= -windowWidth - 2200) {
       ground.x = ground.width / 2;
@@ -160,45 +160,50 @@ function draw() {
       jumpingSound.play();
     }
 
-    //console.log(trex.animation);
+    // console.log(trex.animation);
     generate_clouds();
     generate_cactuses();
 
-    //If player has already been playing for some time && determined frameRate is reached
+    // If player has already been playing for some time && determined frameRate is reached
     if (score >= 300 && frameCount % 60 == 0) {
       //Make game slightly faster the more player plays
       trex.velocity.x = trex.velocity.x * 1.6;
       ground.velocity.x = ground.velocity.x * 1.6;
       objObstacle1.velocity.x = objObstacle1.velocity.x * 1.009;
 
-      //Generate Pterodactylus at random heights for player to dodge
+      // Generate Pterodactylus at random heights for player to dodge
       generate_flyingDino();
 
-      //Day and night cycle 
-      if (time == "day") {
-        //Background will get clearer
-        bg -= 25;
+      // Day and night cycle 
+      if (day == true) {
+        // Background will get clearer
+        bg += 25;
         //Moon will start to fade away
-        moonOpacity+= 25;
-        //It will stay clear for some time...
+        moonOpacity -= 25;
+        // if it's day, then the function ends here
+        // It will stay clear for some time...
         setTimeout(() => {
           if (bg > 0) { return }
-          time = "night";
+          day = false;
         }, 4000)
-        // if it's day, then the function ends here
         return
       }
       
       // if is not day, then it is night
-      //Background will get darker
-      bg += 25;
+      // Background will get darker
+      bg -= 25;
       //Moon will get more visible
-      moonOpacity -= 25;
-      //It will stay dark for some time...
-      setTimeout(() => time = b >= 156 ? "day" : "night", 4000)
+      moonOpacity += 25;
+      // It will stay dark for some time...
+      // corrigir problema no ciclo de dia e noite
+      // It will stay clear for some time...
+      setTimeout(() => {
+        if (bg <= 0) { return }
+        day = true;
+      }, 4000)
     }
 
-    //Trex dies
+    // Trex dies
     if (trex.collides(obstacleGroup) || trex.collides(flyingDinoGroup)) {
       gameOverTxt.show();
 
@@ -211,53 +216,27 @@ function draw() {
   // bug da lua, continua andando mesmo o jogo tendo acabado ou resetado, sua posição não é resetada
   trex.changeAnimation("dead", trexCollide);
 
-  trex.velocity.x = 0;
-  ground.velocity.x = 0;
+  trex.velocity.x = ground.velocity.x = 0;
 
-  //obstacles need to stay in place, otherwise player would collide with them and they would go flying away(no gravity)
-  obstacleGroup.collider = 'static';
+  // obstacles need to stay in place, otherwise player would collide with them and they would go flying away(no gravity)
+  obstacleGroup.collider = flyingDinoGroup.collider = cloudsGroup.collider = 'static';
 
-  obstacleGroup.life = Infinity;
-
-  flyingDinoGroup.collider = 'static';
-
-  flyingDinoGroup.life = Infinity;
-
-  cloudsGroup.collider = 'static';
-
-  cloudsGroup.life = Infinity;
+  obstacleGroup.life = flyingDinoGroup.life = cloudsGroup.life = Infinity;
 
   restart.show();
 
-  document.body.addEventListener("click", () => {
-    //reset Dino's position in case it dies in some weird position
-    trex.position.y = 550;
-    trex.position.x = 80;
-
-    endGame = false;
-
-    time = "day";
-    bg = 156;
-    moonOpacity = 0;
-
-    //reset cactuses, clouds and Pterodactylus generation
-    obstacleGroup.remove();
-
-    flyingDinoGroup.remove();
-
-    cloudsGroup.remove();
-
-    trex.changeAnimation("trexWalk", texTrex);
-    score = 0;
+  document.body.addEventListener("click", restartGame)
+  document.body.addEventListener("keydown", () => {
+    if (endGame) { restartGame() }
   })
 
   maximumScore = score > maximumScore ? score : maximumScore;
 
-  //Save player maximum score so if the page is reloaded player will still have its score record saved
+  // Save player maximum score so if the page is reloaded player will still have its score record saved
   localStorage.setItem("userRecord", maximumScore); 
 }
 
-//Check if Down arrow was released
+// Check if Down arrow was released
 document.body.addEventListener("keyup", e => {
   if (e.key != "ArrowDown" && e.key != "s") { return }
   // if the arrow pressed is down or s:
@@ -265,6 +244,29 @@ document.body.addEventListener("keyup", e => {
   trex.animation.offset.y = 0;
   canJump = true;
 });
+
+function restartGame() {
+  // reset Dino's position in case it dies in some weird position
+  trex.position.y = 550;
+  trex.position.x = 80;
+
+  endGame = false;
+
+  time = "day";
+  bg = 156;
+  moonOpacity = 0;
+
+  // reset cactuses, clouds and Pterodactylus generation
+  obstacleGroup.remove();
+
+  flyingDinoGroup.remove();
+
+  cloudsGroup.remove();
+
+  trex.changeAnimation("trexWalk", texTrex);
+  score = 0;
+  canJump = true;
+}
 
 function generate_clouds() {
   if (frameCount % 60 != 0) { return }
@@ -287,18 +289,18 @@ function generate_cactuses() {
   //
   objObstacle1 = new Sprite(windowWidth + 30, windowHeight - 100, 50, 70);
 
-  //setting cactuses weight to 0 because otherwise, it would filp the floor
+  // setting cactuses weight to 0 because otherwise, it would filp the floor
   objObstacle1.mass = 0;
 
-  //This way cactuses can't collide with Pterodactylus but can with the player
+  // This way cactuses can't collide with Pterodactylus but can with the player
   objObstacle1.collider = 'kinematic';
 
   objObstacle1.velocity.x = -8;
-  //Setting life time to cactuses too
+  // Setting life time to cactuses too
   objObstacle1.life = 180;
 
   obstacleGroup.add(objObstacle1);
-  //randomize which cactus will be generated
+  // randomize which cactus will be generated
   let rng = Math.round(random(1, 6));
   // set image
   objObstacle1.image = `/assets/obstacle${rng}.png`;
