4f7b851bc521452a9d8763aeaf6a9423d4de3ff6
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-08-03 16:49:41 -0300

    Update blink-test.js

--- blink-test.js
+++ blink-test.js
@@ -1,50 +1,62 @@
-let textColor = 255, score = 0;
+/**
+ * Made with p5play!
+ * https://p5play.org
+ */
+
+// Learn more about p5play here -> https://p5play.org/learn
+
+let instructions = 'Tap to create a new sprite, drag to throw it!';
+let box;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-
-    //Defining font style
-    textFont('Fira Code', 27);
+  // creates a canvas that fills the screen
+  new Canvas();
 }
 
 function draw() {
-    background(0);
+  clear(); // try removing this line and see what happens!
+
+  textSize(18);
+  textAlign(CENTER);
+  fill(200);
+  text(instructions, canvas.w / 2, canvas.h / 2);
+
+  if (mouse.presses()) {
+    // by default, sprites collide with other sprites
+    box = new Sprite(mouse.x, mouse.y, 30, 30);
+  }
   
-    //Show player score and score record in screen
-    fill("white");
-    text("Maximum score: " /*+ maximumScore*/, windowWidth - 500, windowHeight - 530);
-    //MaxScore goes before scrore beacuse it will not blink
-    fill(255, 255, 255, textColor);
-    text(score, windowWidth - 1200, windowHeight - 530);
+  if (mouse.dragging()) {
+    box.moveTowards(mouse); // throw the box!
+  }
+  
+  // if the user didn't throw the box,
+  // then give it a random speed and direction
+  if (mouse.released() && !mouse.dragged()) {
+    box.speed = random(0, 5);
+    box.direction = random(0, 360);
+  }
 
-    score += Math.round(getFrameRate() / 60);
-    
-    if (score % 200 == 0) {
-        blink_text();
-    }
+  // by default, all sprites are drawn by p5play
+  // after the end of the draw function
 }
 
-//Make the score text blink
-function blink_text() {
+// WARNING! Using the p5.js web editor is not recommended
+// due to these long-standing issues that they won't fix.
 
-    //let val = 1;
-    textColor = (textColor === 255) ? 0 : 255;
-    //textColor = 0; // Make text invisible
-    // Alterna a cor do texto entre branco e preto
-    /*
-    for(let i = 0; i < 6; i++) {
-        setTimeout(() => {
-            //textColor = (textColor === 255) ? 0 : 255;
-            val = val * -1;
+// 1. When console.log is used to print a number on
+//    every frame, it causes drops in frame rate.
+//    This makes the p5.js web editor unusable
+//    for play testing certain kinds of games.
 
-            if(val == -1) {
-                textColor = 0;
-            }
-            else {
-                textColor = 255;
-            }
-        }, 100)
-        
-    }
-    */
-}
+// 2. console.log doesn't print out the correct
+//    values of arrays before and after they're
+//    changed.
+
+// 3. The sequence mode of loadAnimation doesn't
+//    work unless you use backticks for the name
+//    of the image file.
+
+// p5play is not affiliated with the Processing Foundation.
+// I can't persuade them to fix these critical issues which
+// make the p5.js web editor inadequate for p5play users.
